7ba9c1a7c64f1774f30dbb7ba275181804eef8c8
Elias Borelli <e.borelli@winsysgroup.com>
2022-05-25 18:01:16 -0300

    Add Fluent Validation

---
+++ Sat.Recruitment.Api/Validators/UserValidator.cs
@@ -0,0 +1,32 @@
+﻿using FluentValidation;
+using Sat.Recruitment.Api.Dtos.Request;
+
+namespace Sat.Recruitment.Api.Validators
+{
+    public class UserValidator : AbstractValidator<UserRequestDTO>
+    {
+        public UserValidator()
+        {
+            RuleFor(x => x.Name)
+            .NotEmpty()
+            .NotNull()
+            .WithMessage("The name is required");
+
+            RuleFor(x => x.Address)
+            .NotEmpty()
+            .NotNull()
+            .WithMessage("The address is required");
+
+            RuleFor(x => x.Email)
+            .NotEmpty()
+            .NotNull()
+            .WithMessage("The email is required");
+
+            RuleFor(x => x.Phone)
+            .NotEmpty()
+            .NotNull()
+            .WithMessage("The phone is required");
+        }
+
+    }
+}
