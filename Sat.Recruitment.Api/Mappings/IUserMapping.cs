cb2bd43c3907e4fdb5e1db1f133aa84b3101d351
Elias Borelli <e.borelli@winsysgroup.com>
2022-05-25 18:01:30 -0300

    Add Mappings

---
+++ Sat.Recruitment.Api/Mappings/IUserMapping.cs
@@ -0,0 +1,10 @@
+﻿using Sat.Recruitment.Api.Dtos.Request;
+using Sat.Recruitment.Core.Entities;
+
+namespace Sat.Recruitment.Api.Mappings
+{
+    public interface IUserMapping
+    {
+        User MapFromUserRequestDtoToUserDomain(UserRequestDTO userRequestDTO);
+    }
+}
---
+++ Sat.Recruitment.Api/Mappings/UserMapping.cs
@@ -0,0 +1,31 @@
+﻿using Sat.Recruitment.Api.Dtos.Request;
+using Sat.Recruitment.Core.Entities;
+using Sat.Recruitment.Core.Enums;
+using System;
+
+namespace Sat.Recruitment.Api.Mappings
+{
+    public class UserMapping : IUserMapping
+    {
+        public User MapFromUserRequestDtoToUserDomain(UserRequestDTO userRequestDTO)
+        {
+            var user = new User();
+            switch ((UserType)Enum.Parse(typeof(UserType), userRequestDTO.UserType))
+            {
+                case UserType.SuperUser:
+                    user = new SuperUser() { Address = userRequestDTO.Address, Email = userRequestDTO.Email, Name = userRequestDTO.Name, Phone = userRequestDTO.Phone, Money = userRequestDTO.Money, UserType = UserType.SuperUser };
+                    break;
+                case UserType.Normal:
+                    user = new NormalUser() { Address = userRequestDTO.Address, Email = userRequestDTO.Email, Name = userRequestDTO.Name, Phone = userRequestDTO.Phone, Money = userRequestDTO.Money, UserType = UserType.Normal };
+                    break;
+                case UserType.Premium:
+                    user = new PremiumUser() { Address = userRequestDTO.Address, Email = userRequestDTO.Email, Name = userRequestDTO.Name, Phone = userRequestDTO.Phone, Money = userRequestDTO.Money, UserType = UserType.Premium };
+                    break;
+                default:
+                    break;
+            }
+            return user;
+        }
+
+    }
+}
