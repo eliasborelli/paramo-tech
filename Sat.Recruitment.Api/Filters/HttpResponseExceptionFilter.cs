f5f5d8febe0ca09c2a78680820a14b0674724656
Elias Borelli <e.borelli@winsysgroup.com>
2022-05-25 18:01:46 -0300

    Add Exception Filter.

---
+++ Sat.Recruitment.Api/Filters/HttpResponseExceptionFilter.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Sat.Recruitment.Infraestructure.Commons;
+using System.Linq;
+
+namespace Sat.Recruitment.Api.Filters
+{
+    public class HttpResponseExceptionFilter : IActionFilter, IOrderedFilter
+    {
+        public int Order { get; set; } = int.MaxValue - 10;
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            //
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (!context.ModelState.IsValid)
+            {
+                var errors = context.ModelState.AllErrors().ToList();
+                var result = OperationResponseFactory.CreateResponse("PARAMO_ERROR", "Validation is not correct", errors);
+                context.Result = new BadRequestObjectResult(result);
+            }
+        }
+
+    }
+}
